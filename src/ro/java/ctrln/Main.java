755fbd3fb19bb81a70cf7fe9dcdde75188e1bcc3
AndreeaSimonaC <ciobanu_a_simona@yahoo.com>
2021-01-28 16:51:27 +0200

    Adaugare comentarii pe mai multe linii

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -3,6 +3,9 @@ package ro.java.ctrln;
 public class Main {
     // Comentariu
     //Comentariu2
+    /*Acesta este un comen
+    tariu multi linie
+     */
     public static void main(String[] args) {
         System.out.println("Invat limbajul Java si despre masina virtuala Java");
     }
