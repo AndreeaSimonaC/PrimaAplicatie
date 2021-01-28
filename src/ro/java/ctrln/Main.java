c818a36bb6db0563a5f0dedb6cf0c7b7336c35ad
AndreeaSimonaC <ciobanu_a_simona@yahoo.com>
2021-01-28 16:15:57 +0200

    Adaugare comentarii pe o singura linie

---
+++ src/ro/java/ctrln/Main.java
@@ -0,0 +1,9 @@
+package ro.java.ctrln;
+
+public class Main {
+    // Comentariu
+    //Comentariu2
+    public static void main(String[] args) {
+        System.out.println("Invat limbajul Java si despre masina virtuala Java");
+    }
+}
